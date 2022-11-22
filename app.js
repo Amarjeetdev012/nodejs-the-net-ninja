4e19721a65791b495d203df84811c11349c4486f
unknown <amarjeet@baruzotech.com>
2022-11-22 14:05:40 +0530

    use of how to send data by different methods

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-var fs = require("fs")
-var http = require("http")
+var fs = require("fs");
+var http = require("http");
 
-var server = http.createServer(function(req,res){
-    console.log("request was made by " + req.url)
-    res.writeHead(200, {"Content-Type":"text/html"})
-var myReadStream = fs.createReadStream(__dirname + "/index.html", "utf8")
-    myReadStream.pipe(res)
-})
+var server = http.createServer(function (req, res) {
+  console.log("request was made by " + req.url);
+  res.writeHead(200, { "Content-Type": "application/json" });
+  var myObj = {
+    name: "riyu",
+    job: "ninja",
+    age: 29,
+  };
+  res.end(JSON.stringify(myObj));
+});
 
-server.listen(3000, "127.0.0.1")
+server.listen(3000, "127.0.0.1");
 console.log("yo guys , now listening on port");
-
