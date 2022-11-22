0e2a4de6b105d29a0dff0cd2e0116b2da01e583f
unknown <amarjeet@baruzotech.com>
2022-11-22 10:29:13 +0530

    node event emitter my emitter and utils for inheritence

--- app.js
+++ app.js
@@ -1,6 +1,33 @@
-var stuff = require("./stuff")
+var events = require("events")
 
+// var myEmitter = new events.EventEmitter()
 
-console.log(stuff.counter(["ajsah","afdf","addf","addff"]))
-console.log(stuff.adder(65,8794))
-console.log(stuff.adder(stuff.pi,89))
+// myEmitter.on("someEvent", function(msg){
+//     console.log(msg)
+// })
+
+// myEmitter.emit("someEvent", "the event is emitted")
+
+var util = require("util")
+
+var Person = function (name) {
+    this.name = name
+}
+// inherits like class or type of saancha or eske use se copyies bna sakte hai
+util.inherits(Person, events.EventEmitter)
+
+var james = new Person("james")
+var mary = new Person("mary")
+var ryu = new Person("ryu")
+
+var people = [james,mary,ryu]
+
+people.forEach(function(person){
+    person.on("speak", function (msg) {
+        console.log(person.name + " said "+ msg)
+    })
+})
+
+james.emit("speak" ," hey dudes")
+ryu.emit("speak" ," i want a curry")
+mary.emit("speak" ," i want a fresh mushrooms")
