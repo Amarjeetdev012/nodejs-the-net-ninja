54bb2682b6c3ddb499856b91d43c00980641c1ec
unknown <amarjeet@baruzotech.com>
2022-11-22 09:22:44 +0530

    function expression

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
-console.log(__dirname)
-console.log(__filename)
+//  normal function statement
+
+function sayHi() {
+    console.log("hi")
+}
+
+sayHi()
+
+//  functional expression
+var sayBye = function(){
+    console.log("bye")
+}
+
+sayBye()
+
+// function inside function
+function callFunction(fun){
+    fun()
+}
+
+callFunction(sayBye)
