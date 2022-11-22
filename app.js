b5914b64614e22fdd6d7dc58c35b0c021cf9d6f6
unknown <amarjeet@baruzotech.com>
2022-11-22 12:54:39 +0530

    read data through pipe using streans read

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var fs = require("fs")
+var http = require("http")
 
+var server = http.createServer(function(req,res){
+    console.log("request was made by " + req.url)
+    res.writeHead(200, {"Content-Type":"text/plain"})
 var myReadStream = fs.createReadStream(__dirname + "/readMe.txt", "utf8")
-var myWriteStream = fs.createWriteStream(__dirname + "/writeMe.txt")
-
-myReadStream.on("data", function(chunk){
-    console.log("new chunk received")
-    myWriteStream.write(chunk)
-    console.log("data written succsfully")
+    myReadStream.pipe(res)
 })
+
+server.listen(3000, "127.0.0.1")
+console.log("yo guys , now listening on port");
 
