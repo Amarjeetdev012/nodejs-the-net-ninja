c70148c9749942673e0e4e89d143a440b892ec93
unknown <amarjeet@baruzotech.com>
2022-11-23 14:04:36 +0530

    query and form data accesss and send to or render to post method using

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 const express = require("express");
 const app = express();
+app.use(express.urlencoded({extended:false}));
 
 // for using ejs file we have to set view engine ejs using app.set
 app.set("view engine", "ejs");
 
 // using app.use to serve up static CSS files in public/assets/ folder when /public link is called in ejs files
 // app.use("/route", express.static("foldername"));
-app.use('/stuff', express.static('stuff'));
-
+app.use("/stuff", express.static("stuff"));
 
 app.get("/", function (req, res) {
   res.render("index");
 });
 
 app.get("/contact", function (req, res) {
-  res.render("contact", {qs: req.query});
+  res.render("contact", { qs: req.query });
+});
+
+app.post("/contact", function (req, res) {
+  console.log(req.body);
+  res.render("contact-response", { data: req.body });
 });
 
 app.get("/profile/:name", function (req, res) {
